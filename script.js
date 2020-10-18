1ef31d10de5f60324544039af0c0201022eb040e
szabolevi98 <46320470+szabolevi98@users.noreply.github.com>
2020-10-18 18:35:12 +0200

    2020.10.18.

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 window.addEventListener('load', makeChart);
 
+let config = {
+  legend: {
+    fontColor: "white",
+    labels: {
+      fontColor: 'white'
+    }
+  },
+  scales: {
+    xAxes: [{
+      ticks: {
+        fontColor: "white",
+      }
+    }],
+    yAxes: [{
+      ticks: {
+        fontColor: "white",
+      }
+    }]
+  }
+};
+
 async function makeChart() {
   const ctx = document.getElementById('myChart').getContext('2d');
+  const ctxFest = document.getElementById('myChartFest').getContext('2d');
+  const ctxElection = document.getElementById('myChartElection').getContext('2d');
   const data = await getData();
   const myChart = new Chart(ctx, {
     type: 'line',
@@ -27,26 +50,42 @@ async function makeChart() {
         }
       ]
     },
-    options: {
-      legend: {
-        fontColor: "white",
-        labels: {
-          fontColor: 'white'
+    options: config
+  });
+  const myChartFest = new Chart(ctxFest, {
+    type: 'bar',
+    data: {
+      labels: Object.keys(data.octoberFest),
+      datasets: [
+        {
+          label: 'OctoberFest',
+          data: Object.values(data.octoberFest),
+          fill: false,
+          borderColor: 'rgba(156, 245, 66, 1)',
+          backgroundColor: 'rgba(156, 245, 66, 0.5)',
+          fontColor: 'white',
+          borderWidth: 2
         }
-      },
-      scales: {
-        xAxes: [{
-          ticks: {
-            fontColor: "white",
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            fontColor: "white",
-          }
-        }]
-      }
-    }
+      ]
+    },
+    options: config
+  });
+  const myChartElection = new Chart(ctxElection, {
+    type: 'bar',
+    data: {
+      labels: Object.keys(data.federalElection),
+      datasets: [
+        {
+          label: 'Federal Election',
+          data: Object.values(data.federalElection),
+          fill: false,
+          borderColor: 'rgba(66, 218, 245, 1)',
+          backgroundColor: 'rgba(66, 218, 245, 0.5)',
+          borderWidth: 2
+        }
+      ]
+    },
+    options: config
   });
 }
 
@@ -55,12 +94,9 @@ async function getData() {
   const data = (await response.text()).trim();
   //const years = [];
   const octoberFest = {}, federalElection = {};
-  const rows = data.split('\n').slice(1);
-  rows.forEach(row => {
+  data.split('\n').slice(1).forEach(row => {
     const fields = row.split(';');
-    let year = fields[0];
-    let festNum = fields[3];
-    let electionNum = fields[4];
+    let year = fields[0], festNum = fields[3], electionNum = fields[4];
     //years.push(year);
     octoberFest[year] = (octoberFest[year] || 0) + Number(festNum); //A '|| 0' a legelső érték miatt kell mert undefined + number = NaN
     federalElection[year] = (federalElection[year] || 0) + Number(electionNum);
